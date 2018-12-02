454c87c958039f9332f4d2df942521c217c61a4e
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2018-12-02 20:54:50 +0300

    implement create feature set handler

--- bundles/ru.arsysop.passage.loc.products.emfforms/src/ru/arsysop/passage/loc/products/emfforms/CreateFeatureSetHandler.java
+++ bundles/ru.arsysop.passage.loc.products.emfforms/src/ru/arsysop/passage/loc/products/emfforms/CreateFeatureSetHandler.java
@@ -2,11 +2,38 @@
 package ru.arsysop.passage.loc.products.emfforms;
 
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
+
+import ru.arsysop.passage.lic.base.ui.LicensingImages;
+import ru.arsysop.passage.lic.model.meta.LicPackage;
+import ru.arsysop.passage.loc.edit.FeatureDomainRegistry;
+import ru.arsysop.passage.loc.edit.ProductDomainRegistry;
+import ru.arsysop.passage.loc.workbench.emfforms.CreateFormWizard;
 
 public class CreateFeatureSetHandler {
+
 	@Execute
-	public void execute() {
-		
+	public void execute(Shell shell, LicensingImages images, FeatureDomainRegistry registry) {
+		LicPackage ePackage = LicPackage.eINSTANCE;
+		EClass eClass = ePackage.getFeatureSet();
+		EObject eObject = ePackage.getEFactoryInstance().create(eClass);
+		String newText = "New Feature Set";
+		String newTitle = "Feature Set";
+		String newMessage = "Please specify a file name to store feature data";
+
+		Wizard wizard = new CreateFormWizard(registry, eObject);
+		WizardDialog dialog = new WizardDialog(shell, wizard);
+		dialog.create();
+		dialog.setTitle(newTitle);
+		dialog.setMessage(newMessage);;
+		Shell createdShell = dialog.getShell();
+		createdShell.setText(newText);
+		createdShell.setImage(images.getImage(eClass.getName()));
+		dialog.open();
 	}
 		
 }
