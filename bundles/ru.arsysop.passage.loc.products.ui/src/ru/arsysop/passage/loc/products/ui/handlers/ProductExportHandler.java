cd831cca7b824160d734e9279b075d87892a825e
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2018-12-03 16:21:00 +0300

    implement product key export

--- bundles/ru.arsysop.passage.loc.products.ui/src/ru/arsysop/passage/loc/products/ui/handlers/ProductExportHandler.java
+++ bundles/ru.arsysop.passage.loc.products.ui/src/ru/arsysop/passage/loc/products/ui/handlers/ProductExportHandler.java
@@ -20,13 +20,89 @@
  *******************************************************************************/
 package ru.arsysop.passage.loc.products.ui.handlers;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.inject.Named;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+
+import ru.arsysop.passage.lic.model.api.Product;
+import ru.arsysop.passage.lic.model.api.ProductVersion;
+import ru.arsysop.passage.lic.runtime.io.StreamCodec;
+import ru.arsysop.passage.loc.edit.ProductDomainRegistry;
+import ru.arsysop.passage.loc.products.core.ProductsCore;
+import ru.arsysop.passage.loc.workbench.LocWokbench;
 
 public class ProductExportHandler {
-	
+
 	@Execute
-	public void execute() {
-		
+	public void execute(@Named(IServiceConstants.ACTIVE_SELECTION) ProductVersion productVersion, Shell shell, ProductDomainRegistry registry, StreamCodec streamCodec) {
+		String installationToken = productVersion.getInstallationToken();
+		if (installationToken != null) {
+			File publicFile = new File(installationToken);
+			if (publicFile.exists()) {
+				String pattern = "Public key already defined: \n %s";
+				MessageDialog.openError(shell, "Error", String.format(pattern, publicFile.getAbsolutePath()));
+				return;
+			}
+		}
+		String secureToken = productVersion.getSecureToken();
+		if (secureToken != null) {
+			File privateFile = new File(secureToken);
+			if (privateFile.exists()) {
+				String pattern = "Private key already defined: \n %s";
+				MessageDialog.openError(shell, "Error", String.format(pattern, privateFile.getAbsolutePath()));
+				return;
+			}
+		}
+
+		Product product = productVersion.getProduct();
+		String errors = LocWokbench.extractValidationError(product);
+		if (errors != null) {
+			MessageDialog.openError(shell, "Error", errors);
+			return;
+		}
+		try {
+			String identifier = product.getIdentifier();
+			String version = productVersion.getVersion();
+			Path basePath = registry.getBasePath();
+			Path path = basePath.resolve(identifier).resolve(version);
+			Files.createDirectories(path);
+			String storageKeyFolder = path.toFile().getAbsolutePath();
+			String keyFileName = identifier + '_' + version;
+			String publicKeyPath = storageKeyFolder + File.separator + keyFileName + ".pub";
+			String privateKeyPath = storageKeyFolder + File.separator + keyFileName + ".skr";
+			streamCodec.createKeyPair(publicKeyPath, privateKeyPath, product.getName(),
+					registry.createPassword(productVersion), 1024);
+			productVersion.setInstallationToken(publicKeyPath);
+			productVersion.setSecureToken(privateKeyPath);
+
+			String format = "Product keys exported succesfully: \n\n %s \n %s \n";
+			String message = String.format(format, publicKeyPath, privateKeyPath);
+			MessageDialog.openInformation(shell, "Product Key Export", message);
+
+		} catch (Exception e) {
+			// TODO: log
+			e.printStackTrace();
+			IStatus error = new Status(IStatus.ERROR, ProductsCore.BUNDLE_SYMBOLIC_NAME, "Product key export error", e);
+			ErrorDialog.openError(shell, "Error", "Error during product key export", error);
+		}
 	}
-		
+
+	@CanExecute
+	public boolean canExecute(@Named(IServiceConstants.ACTIVE_SELECTION) @Optional ProductVersion productVersion) {
+		return productVersion != null;
+	}
+
 }
--- bundles/ru.arsysop.passage.loc.workbench/src/ru/arsysop/passage/loc/workbench/LocWokbench.java
+++ bundles/ru.arsysop.passage.loc.workbench/src/ru/arsysop/passage/loc/workbench/LocWokbench.java
@@ -23,12 +23,15 @@ package ru.arsysop.passage.loc.workbench;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.Diagnostician;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 
 public class LocWokbench {
-	
+
 	public static String selectSavePath(Shell shell, String extension) {
 		String[] array = maskFilters(extension);
 		FileDialog fileDialog = new FileDialog(shell, SWT.SAVE);
@@ -52,9 +55,36 @@ public class LocWokbench {
 		String[] array = (String[]) filters.toArray(new String[filters.size()]);
 		return array;
 	}
-	
+
 	private static String maskExtension(String extension) {
 		return "*." + extension; //$NON-NLS-1$
+	}
+
+	public static String extractValidationError(EObject newObject) {
+		if (newObject == null) {
+			return "Input is invalid";
+		}
+		final Diagnostic result = Diagnostician.INSTANCE.validate(newObject);
+		if (result.getSeverity() == Diagnostic.OK) {
+			return null;
+		}
+		// Get the error count and create an appropriate Error message:
+		final int errorCount = result.getChildren().size();
+		
+		final String header = "%s error(s) occured while analyzing your inputs:";
+		final String entry = "%s. %s";
+		
+		final StringBuilder sb = new StringBuilder();
+		sb.append(String.format(header, errorCount));
+		sb.append('\n');
+
+		int messageCount = 0;
+		for (final Diagnostic d : result.getChildren()) {
+			sb.append('\n');
+			sb.append(String.format(entry, ++messageCount, d.getMessage()));
+		}
+		
+		return sb.toString();
 	}
 
 }
