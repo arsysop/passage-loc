93a80d25432aa3c3ad59eac8ca0d5db5dcd01221
Sergey Kovalchuk <serjiokov@gmail.com>
2018-12-17 00:43:08 +0300

    Added dialog for expression representation

---
+++ bundles/ru.arsysop.passage.loc.licenses.emfforms/src/ru/arsysop/passage/loc/licenses/emfforms/renders/LicenseExpressionRenderer.java
@@ -0,0 +1,220 @@
+package ru.arsysop.passage.loc.licenses.emfforms.renders;
+
+import javax.inject.Inject;
+
+import org.eclipse.core.databinding.Binding;
+import org.eclipse.core.databinding.UpdateValueStrategy;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecp.view.spi.context.ViewModelContext;
+import org.eclipse.emf.ecp.view.spi.core.swt.SimpleControlSWTControlSWTRenderer;
+import org.eclipse.emf.ecp.view.spi.model.VControl;
+import org.eclipse.emf.ecp.view.template.model.VTViewTemplateProvider;
+import org.eclipse.emfforms.spi.common.report.ReportService;
+import org.eclipse.emfforms.spi.core.services.databinding.DatabindingFailedException;
+import org.eclipse.emfforms.spi.core.services.databinding.EMFFormsDatabinding;
+import org.eclipse.emfforms.spi.core.services.label.EMFFormsLabelProvider;
+import org.eclipse.jface.databinding.swt.WidgetProperties;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+import ru.arsysop.passage.lic.model.api.LicenseGrant;
+
+public class LicenseExpressionRenderer extends SimpleControlSWTControlSWTRenderer {
+
+	ExpressionDioalog dialog;
+	Composite base;
+	Text txtConditionExpression;
+	String result;
+
+	@Inject
+	public LicenseExpressionRenderer(VControl vElement, ViewModelContext viewContext, ReportService reportService,
+			EMFFormsDatabinding emfFormsDatabinding, EMFFormsLabelProvider emfFormsLabelProvider,
+			VTViewTemplateProvider vtViewTemplateProvider) {
+		super(vElement, viewContext, reportService, emfFormsDatabinding, emfFormsLabelProvider, vtViewTemplateProvider);
+
+	}
+
+	@Override
+	protected Binding[] createBindings(Control control) throws DatabindingFailedException {
+		if (control instanceof Text) {
+			final Binding binding = getDataBindingContext().bindValue(
+					WidgetProperties.text(SWT.Modify).observe(txtConditionExpression), getModelValue(),
+					withPreSetValidation(new UpdateValueStrategy()), null);
+			return new Binding[] { binding };
+		}
+
+		return new Binding[] {};
+	}
+
+	@Override
+	protected Control createSWTControl(Composite parent) {
+		base = new Composite(parent, SWT.NONE);
+		base.setLayout(new GridLayout(2, false));
+		base.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
+
+		GridData txtGD = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1);
+		txtConditionExpression = new Text(base, SWT.BORDER);
+		txtConditionExpression.setLayoutData(txtGD);
+		txtConditionExpression.setText(getCurrentValue());
+		txtConditionExpression.setEditable(false);
+		GridData btnGD = new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1);
+		btnGD.heightHint = 28;
+		btnGD.widthHint = 48;
+		Button btnConditionExpressionEdit = new Button(base, SWT.PUSH);
+		btnConditionExpressionEdit.setText("Edit");
+		btnConditionExpressionEdit.setLayoutData(btnGD);
+		btnConditionExpressionEdit.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				dialog = new ExpressionDioalog(Display.getDefault().getActiveShell(), getCurrentValue());
+				if (dialog.open() == Dialog.OK) {
+					txtConditionExpression.setText(dialog.getResultValues());
+				}
+			}
+		});
+
+		return txtConditionExpression;
+	}
+
+	@Override
+	protected void dispose() {
+		super.dispose();
+		if (base != null && !base.isDisposed()) {
+			for (Control control : base.getChildren()) {
+				if (control != null) {
+					control.dispose();
+				}
+			}
+		}
+
+	}
+
+	protected String getCurrentValue() {
+		String conditionType = "";
+		EObject domainModel = getViewModelContext().getDomainModel();
+		if (domainModel instanceof LicenseGrant) {
+			conditionType = ((LicenseGrant) domainModel).getConditionExpression();
+		}
+		if (conditionType == null) {
+			conditionType = "";
+		}
+		return conditionType;
+	}
+
+	@Override
+	protected String getUnsetText() {
+		return "";
+	}
+
+	class ExpressionDioalog extends Dialog {
+		List lstItems;
+		String values;
+		Text editItem;
+		Button addItem;
+
+		protected ExpressionDioalog(Shell parentShell, String values) {
+			super(parentShell);
+			this.values = values;
+		}
+
+		public String getResultValues() {
+			return result;
+		}
+
+		private String prepareResultValues() {
+			StringBuilder builder = new StringBuilder();
+			for (String item : lstItems.getItems()) {
+				if (builder.length() > 0) {
+					builder.append(";");
+				}
+				builder.append(item);
+			}
+			return builder.toString();
+		}
+
+		@Override
+		protected void buttonPressed(int buttonId) {
+			if (buttonId == Dialog.OK) {
+				result = prepareResultValues();
+			}
+			super.buttonPressed(buttonId);
+		}
+
+		@Override
+		protected Control createDialogArea(Composite parent) {
+			parent.setLayout(new GridLayout(1, false));
+			Composite base = new Composite(parent, SWT.BORDER);
+			base.setLayout(new GridLayout(3, false));
+			base.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+
+			GridData text = new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1);
+			Text txtExpressionItem = new Text(base, SWT.BORDER);
+			txtExpressionItem.setLayoutData(text);
+			txtExpressionItem.setText("");
+			GridData btn = new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1);
+			btn.heightHint = 28;
+			btn.widthHint = 48;
+
+			Button btnConditionExpressionEdit = new Button(base, SWT.PUSH);
+			btnConditionExpressionEdit.setText("Add");
+			btnConditionExpressionEdit.setLayoutData(btn);
+			btnConditionExpressionEdit.addSelectionListener(new SelectionAdapter() {
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					String textExpression = txtExpressionItem.getText();
+					if (!textExpression.isEmpty()) {
+						lstItems.add(textExpression);
+						txtExpressionItem.setText("");
+					}
+				}
+			});
+			Button btnConditionExpressionRemove = new Button(base, SWT.PUSH);
+			btnConditionExpressionRemove.setText("Remove");
+			btnConditionExpressionRemove.setLayoutData(btn);
+			btnConditionExpressionRemove.addSelectionListener(new SelectionAdapter() {
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					int selectionIndex = lstItems.getSelectionIndex();
+					if (selectionIndex > -1) {
+						lstItems.remove(selectionIndex);
+						txtExpressionItem.setText("");
+					}
+				}
+			});
+
+			lstItems = new List(base, SWT.BORDER);
+			lstItems.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 3, 1));
+
+			if (values != null && !values.isEmpty()) {
+				String[] splitedVAlues = values.split(";");
+				lstItems.setItems(splitedVAlues);
+			}
+
+			lstItems.addSelectionListener(new SelectionAdapter() {
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					String selectedValue = lstItems.getItems()[lstItems.getSelectionIndex()];
+					txtExpressionItem.setText(selectedValue);
+				}
+			});
+
+			return parent;
+		}
+
+		@Override
+		protected boolean isResizable() {
+			return true;
+		}
+	}
+}
