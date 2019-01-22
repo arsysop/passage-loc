ac7e57e6084e8687cf6bf49e1784475f3b13cb85
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2019-01-22 17:31:02 +0300

    add lic.emf.edit

--- bundles/ru.arsysop.passage.loc.edit/src/ru/arsysop/passage/lic/emf/edit/SelectionCommandAdvisor.java
+++
@@ -1,8 +0,0 @@
-package ru.arsysop.passage.lic.emf.edit;
-
-public interface SelectionCommandAdvisor {
-
-	String getSelectionTitle(String classifier);
-
-	Iterable<?> getSelectionInput(String classifier);
-}
